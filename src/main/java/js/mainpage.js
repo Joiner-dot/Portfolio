3d8c0fc338babf0f3bafa4a1e4760aa31dfae24d
George <egor_kuzmin22@mail.ru>
2020-11-18 12:02:02 +0300

    Commit

--- src/main/java/js/mainpage.js
+++ src/main/java/js/mainpage.js
@@ -1,30 +1,46 @@
 function size() {
     document.getElementById('news').onmouseover = function () {
-        this.style = " transform: scale(1.2); transition:1s;"
+        this.style.transform = "scale(1.2)"
+        this.style.transition = "1s"
+        this.style.cursor = "pointer"
     }
+
     document.getElementById('news').onmouseleave = function () {
-        this.style = "width:20%; height:10vmax; transition:1s;"
+        this.style.transform = "scale(1)"
+        this.style.transition = "1s"
     }
 
     document.getElementById('aboutus').onmouseover = function () {
-        this.style = "transform: scale(1.2); transition:1s;"
+        this.style.transform = "scale(1.2)"
+        this.style.transition = "1s"
+        this.style.cursor = "pointer"
     }
+
     document.getElementById('aboutus').onmouseleave = function () {
-        this.style = "width:20%; height:10vmax; transition:1s;"
+        this.style.transform = "scale(1)"
+        this.style.transition = "1s"
     }
 
     document.getElementById('interview').onmouseover = function () {
-        this.style = "transform: scale(1.2); transition:1s;"
+        this.style.transform = "scale(1.2)"
+        this.style.transition = "1s"
+        this.style.cursor = "pointer"
     }
+
     document.getElementById('interview').onmouseleave = function () {
-        this.style = "width:20%; height:10vmax; transition:1s;"
+        this.style.transform = "scale(1)"
+        this.style.transition = "1s"
     }
 
     document.getElementById('contacts').onmouseover = function () {
-        this.style = "transform: scale(1.2); transition:1s;"
+        this.style.transform = "scale(1.2)"
+        this.style.transition = "1s"
+        this.style.cursor = "pointer"
     }
+
     document.getElementById('contacts').onmouseleave = function () {
-        this.style = "width:20%; height:10vmax; transition:1s;"
+        this.style.transform = "scale(1)"
+        this.style.transition = "1s"
     }
 
     document.getElementById("contacts").onclick = function () {
